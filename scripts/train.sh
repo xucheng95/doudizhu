bcb4889461f1d71bdaf11d1ebc58be2ceb2d69ab
cheng.xu <cheng.xu@happyelements.com>
2026-06-10 14:10:26 +0800

    fix: train.sh去掉硬编码python路径

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")/.."
-/Library/Developer/CommandLineTools/Library/Frameworks/Python3.framework/Versions/3.9/bin/python3.9 -m training.train --config configs/default.yaml "$@"
+python3 -m training.train --config configs/default.yaml "$@"
